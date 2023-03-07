fbeed58c30e03cb626f0d30132ad33da93b0307a
tenandtak <114275420+tenandtak@users.noreply.github.com>
2023-03-07 21:22:14 +0900

    Updated code.

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ import pandas
 st.set_page_config(layout="wide")
 
 st.title("Welcome to My Portfolio Apps Site!!😄")
-st.write("🍏😺🍏🐈🍏🐾🍏🐱🍏🐈🍏🐾🍏😸🍏🐈🍏🐾🍏😽🍏🐈🍏🐾🍏😹🍏🐈🍏🐾🍏😺🍏🐈🍏🐾🍏🐱🍏🐈🍏🐾🍏😸🍏🐈🍏🐾🍏😽🍏🐈🍏🐾🍏😹🍏🐈🍏")
+st.write("🍏😺🍏🐈🍏🐾🍏🐱🍏🐈🍏🐾🍏😸🍏🐈🍏🐾🍏😽🍏🐈🍏🐾🍏😹🍏🐈🍏🐾🍏😺🍏🐈🍏🐾🍏🐱🍏🐈🍏🐾🍏😸")
 st.write("")
 
 column_1, column_2 = st.columns(2)
@@ -23,7 +23,7 @@ with column_2:
     st.info(content)
 
 content2 = """
-There are 20 project icons below, however, currently I've finished four projects; 1) Todo App, 2) Portfolio Website (this one:-)), 3) PDF Templates, and 4) PDF Invoices.
+There are 20 project icons below you can see, however, currently I've finished four projects; 1) Todo App, 2) Portfolio Website (this one:-)), 3) PDF Templates, and 4) PDF Invoices.
 I haven't started other projects, but if there is any project you want to see how I can do it, you can drop me a message from the Contact Me shown in the left pane to let me know:)
 """
 # st.text(content2) # This was my solution!
@@ -38,16 +38,40 @@ with column_3:
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
-
+        if row['url'] == "None":
+            st.write("Coming Soon")
+        else:
+            st.write(f"[Source Code]({row['url']})")
+        if row['url2'] == "None":
+            st.write("")
+        elif row['url2'] == "CLI only":
+            st.write("CLI only")
+        else:
+            st.write(f"[Web App]({row['url2']})")
+        if row['exe'] == True:
+            binary_file = "gui.exe"
+            with open(binary_file, "rb") as file:
+                btn = st.download_button(
+                    label="Desktop App",
+                    data=file,
+                    file_name="gui.exe",
+                    mime="application/octet-stream")
+        else:
+            st.write("")
 
 with column_4:
     for index, row in dataframe[10:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
-
-
-
+        if row['url'] == "None":
+            st.write("Coming Soon")
+        else:
+            st.write(f"[Source Code]({row['url']})")
+        if row['url2'] == "None":
+            st.write("")
+        elif row['url2'] == "CLI only":
+            st.write("CLI only")
+        else:
+            st.write(f"[App Link]({row['url2']})")
 
